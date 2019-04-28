b54e73888bc0e34f226e7de5c740c647c239403a
= <=>
2019-04-28 13:24:17 +0300

    Add nullptr

--- Nullptr/Nullptr.cpp
+++ Nullptr/Nullptr.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 /*
- * Nullptr 
+ * Nullptr
  * To distinguish between an integer 0 and a null pointer.
  * It's an example of return type resolver idiom, which is used
  * to implement nullptr.
+ * Official proposal:
+ * http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2007/n2431.pdf
  */
 
 const 
@@ -12,22 +14,36 @@ class NullptrT
 {
 public:
     template<typename T>
-    operator T*() const { return 0; }
+    operator T*() const { return 0; } // for non-member pointer
 
     template<typename C, typename T>
-    operator T C::*() const { return 0; }
-
-    operator bool() const { return false; }
+    operator T C::*() const { return 0; } // for member pointer
 private:
-    void operator&() const;    
+    void operator&() const;  
 } Nullptr;
+
+struct X {
+    void f1(){}
+};
+
+void foo(int) { std::cout << "foo int" << std::endl; }
+void foo(char*) { std::cout << "foo char*" << std::endl; }
 
 int main()
 {
-    int * p = Nullptr;
+    int *p1 = Nullptr;
+    if(not p1) std::cout << "p1 is null" << std::endl;
+    if(p1 == Nullptr) std::cout << "p1 is null" << std::endl;
 
-    if(not p) std::cout << "p is null" << std::endl; 
-    if(not Nullptr) std::cout << "nullptr is null" << std::endl; 
+    int x = 10;
+    int *p2 = &x;
+    if(p2 != Nullptr) std::cout << "p2 is not null" << std::endl;
+
+    void (X::*fptr)() = Nullptr;
+
+    foo(Nullptr);
+    foo(nullptr);
+    //foo(NULL);
 
     return 0;
 }
